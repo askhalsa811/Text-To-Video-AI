0006240789892474df2e5007ebfa2bddc7af4a89
askhalsa811 <56396873+askhalsa811@users.noreply.github.com>
2024-10-15 07:52:41 -0400

    Update audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,10 +1,9 @@
 import edge_tts
 
-async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+async def generate_audio(text, outputFilename):
+    # Wrapping the text in SSML tags to reduce the speech rate to 75% (25% slower)
+    ssml_text = f'<speak><prosody rate="-25%">{text}</prosody></speak>'
+    
+    # Generating the audio with the modified SSML text
+    communicate = edge_tts.Communicate(ssml_text, "en-AU-WilliamNeural")
     await communicate.save(outputFilename)
-
-
-
-
-
